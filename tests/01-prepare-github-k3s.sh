ddc9fa7f65f43e5be5c53304208a1c478385d62a
Dmitrii Ermakov <dmitrii.ermakov@maxiv.lu.se>
2023-04-18 22:20:46 +0200

    Tests: fix step 01
    Signed-off-by: Dmitrii Ermakov <dmitrii.ermakov@maxiv.lu.se>

--- tests/01-prepare-github-k3s.sh
+++ tests/01-prepare-github-k3s.sh
@@ -16,27 +16,32 @@ curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack
 cp kustomize /usr/local/bin/kustomize
 
 echo "Installing cert-manager for webhook"
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.8.2/cert-manager.yaml
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.11.0/cert-manager.yaml
 kubectl --namespace cert-manager rollout status deployment/cert-manager-cainjector --timeout=60s
 kubectl --namespace cert-manager rollout status deployment/cert-manager --timeout=60s
 kubectl --namespace cert-manager rollout status deployment/cert-manager-webhook --timeout=60s
 
 echo "Downloading Multus CNI"
 # Special thanks to: https://gist.github.com/janeczku/ab5139791f28bfba1e0e03cfc2963ecf
-curl -L "https://raw.githubusercontent.com/k8snetworkplumbingwg/multus-cni/v3.9/deployments/multus-daemonset.yml" -o multus-cni.yaml
-sed -i -e 's/path: \/etc\/cni\/net.d/path: \/var\/lib\/rancher\/k3s\/agent\/etc\/cni\/net.d/' multus-cni.yaml
-sed -i -e 's/path: \/opt\/cni\/bin/path: \/var\/lib\/rancher\/k3s\/data\/current\/bin/' multus-cni.yaml
-sed -i -e 's/args:/args:\n        - "--multus-kubeconfig-file-host=\/var\/lib\/rancher\/k3s\/agent\/etc\/cni\/net.d\/multus.d\/multus.kubeconfig"/' multus-cni.yaml
-kubectl apply --wait -f multus-cni.yaml
+# The multus-cni.yml is curl -L "https://raw.githubusercontent.com/k8snetworkplumbingwg/multus-cni/v3.9/deployments/multus-daemonset.yml" -o multus-cni.yaml
+# with several changed paths to work with K3S:
+# volumes:
+#   - name: cni
+#     hostPath:
+#       path: /var/lib/rancher/k3s/agent/etc/cni/net.d/
+#   - name: cnibin
+#     hostPath:
+#       path: /var/lib/rancher/k3s/data/current/bin/
+# sed -i -e 's/path: \/etc\/cni\/net.d/path: \/var\/lib\/rancher\/k3s\/agent\/etc\/cni\/net.d/' multus-cni.yaml
+# sed -i -e 's/path: \/opt\/cni\/bin/path: \/var\/lib\/rancher\/k3s\/data\/current\/bin/' multus-cni.yaml
+# sed -i -e 's/args:/args:\n        - "--multus-kubeconfig-file-host=\/var\/lib\/rancher\/k3s\/agent\/etc\/cni\/net.d\/multus.d\/multus.kubeconfig"/' multus-cni.yaml
+kubectl apply --wait -f tests/multus-cni.yaml
 kubectl -n kube-system rollout status  daemonset/kube-multus-ds --timeout=120s
 
 
 echo "Installing Linkerd CLI"
 curl --proto '=https' --tlsv1.2 -sSfL https://run.linkerd.io/install | sh
 linkerd version
-
-echo "Check Kubernetes cluster before Linkerd install"
-linkerd check --pre
 
 # Use /tmp/ for linkerd-cni configuration so as to 
 # prevent kubelet from handling linked cni via its configuration file.
