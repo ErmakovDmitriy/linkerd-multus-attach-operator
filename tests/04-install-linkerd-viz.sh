e5432fcb067e8b8297b81392363eda90615a58bb
Dmitrii Ermakov <dmitrii.ermakov@maxiv.lu.se>
2023-04-19 10:18:42 +0200

    Add diagnostics to linkerd-viz test

--- tests/04-install-linkerd-viz.sh
+++ tests/04-install-linkerd-viz.sh
@@ -8,9 +8,17 @@ NAMESPACE="linkerd-viz"
 kubectl create namespace $NAMESPACE
 kubectl annotate --overwrite namespace/$NAMESPACE "linkerd.io/multus=enabled"
 
-linkerd viz install | kubectl apply -f -
+echo "Print extra diagnostics information"
+kubectl get ns $NAMESPACE -o yaml
+kubectl -n $NAMESPACE get network-attachment-definitions.k8s.cni.cncf.io linkerd-cni -o yaml
+
+linkerd viz install | kubectl apply --wait -f -
 # If this check is successful, it means that the multus handles linkerd-cni
 # plugin as expected. As soon as the multus annotation (and as a result the Multus NetworkAttachmentDefinition)
 # is removed from the linkerd-viz namespace, the check fails because the viz
 # pods can not communicate to linkerd control plane.
+kubectl -n $NAMESPACE get pod
+kubectl -n $NAMESPACE get event
+
+echo "Checking Linkerd VIZ"
 linkerd viz check
